977cf4dd4cc9059ddc05c9035e6f4c2bffcc677f
Alisha Foxall <Alisha Foxall>
2017-04-02 18:54:51 -0400

    working on adding images, nt working yet tho

--- inc/class.recipes.inc.php
+++ inc/class.recipes.inc.php
@@ -252,7 +252,7 @@ class RecipeManager{
 	}
 	
 	/*
-	* Adds a new recipe 
+	* Adds a new recipe
 	*/
 
 	public function addRecipe(){
@@ -324,6 +324,46 @@ class RecipeManager{
             }
         }
 
+        $targetDir = "images/";
+        $target_file = $targetDir . basename($_FILES["imageFile"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+        $num = 0;
+        while (file_exists($target_file . $num)) {
+            $num += 1;
+
+        }
+        $target_file .= $num;
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            $tmp = "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            $tmp = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            $tmp = "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                $tmp = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            } else {
+                $tmp = "Sorry, there was an error uploading your file.";
+            }
+        }
         return $tmp;
 
 	}
